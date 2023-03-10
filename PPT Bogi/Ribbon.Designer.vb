4651190b8ee229b14c771644cc3be2ca66c91257
KovarJoel <joel.m.kovar@gmail.com>
2023-03-10 21:00:30 +0100

    Add project files.

---
+++ PPT Bogi/Ribbon.Designer.vb
@@ -0,0 +1,117 @@
+﻿Partial Class Ribbon
+    Inherits Microsoft.Office.Tools.Ribbon.RibbonBase
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New(ByVal container As System.ComponentModel.IContainer)
+        MyClass.New()
+
+        'Required for Windows.Forms Class Composition Designer support
+        If (container IsNot Nothing) Then
+            container.Add(Me)
+        End If
+
+    End Sub
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New(Globals.Factory.GetRibbonFactory())
+
+        'This call is required by the Component Designer.
+        InitializeComponent()
+
+    End Sub
+
+    'Component overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim RibbonDialogLauncherImpl1 As Microsoft.Office.Tools.Ribbon.RibbonDialogLauncher = Me.Factory.CreateRibbonDialogLauncher
+        Me.TabBogi = Me.Factory.CreateRibbonTab
+        Me.GroupLieder = Me.Factory.CreateRibbonGroup
+        Me.ButtonInsertSong = Me.Factory.CreateRibbonButton
+        Me.ButtonChangeDirectory = Me.Factory.CreateRibbonButton
+        Me.TabBogi.SuspendLayout()
+        Me.GroupLieder.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabBogi
+        '
+        Me.TabBogi.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office
+        Me.TabBogi.Groups.Add(Me.GroupLieder)
+        Me.TabBogi.Label = "PPT Bogi"
+        Me.TabBogi.Name = "TabBogi"
+        '
+        'GroupLieder
+        '
+        RibbonDialogLauncherImpl1.ScreenTip = "Hilfe"
+        RibbonDialogLauncherImpl1.SuperTip = "Bei Problemen oder Fragen."
+        Me.GroupLieder.DialogLauncher = RibbonDialogLauncherImpl1
+        Me.GroupLieder.Items.Add(Me.ButtonInsertSong)
+        Me.GroupLieder.Items.Add(Me.ButtonChangeDirectory)
+        Me.GroupLieder.Label = "Lieder"
+        Me.GroupLieder.Name = "GroupLieder"
+        '
+        'ButtonInsertSong
+        '
+        Me.ButtonInsertSong.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
+        Me.ButtonInsertSong.Image = Global.PPT_Bogi.My.Resources.Resources.Song
+        Me.ButtonInsertSong.Label = "Lied einfügen"
+        Me.ButtonInsertSong.Name = "ButtonInsertSong"
+        Me.ButtonInsertSong.ScreenTip = "Lied einfügen"
+        Me.ButtonInsertSong.ShowImage = True
+        Me.ButtonInsertSong.SuperTip = "Fügt ein Lied an der aktuellen Position ein."
+        '
+        'ButtonChangeDirectory
+        '
+        Me.ButtonChangeDirectory.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
+        Me.ButtonChangeDirectory.Image = Global.PPT_Bogi.My.Resources.Resources.Folder
+        Me.ButtonChangeDirectory.Label = "Verzeichnis ändern"
+        Me.ButtonChangeDirectory.Name = "ButtonChangeDirectory"
+        Me.ButtonChangeDirectory.ScreenTip = "Verzeichnis ändern"
+        Me.ButtonChangeDirectory.ShowImage = True
+        Me.ButtonChangeDirectory.SuperTip = "Ändert das Verzeichnis, aus welchem die Lieder geöffnet werden sollen."
+        '
+        'Ribbon
+        '
+        Me.Name = "Ribbon"
+        Me.RibbonType = "Microsoft.PowerPoint.Presentation"
+        Me.Tabs.Add(Me.TabBogi)
+        Me.TabBogi.ResumeLayout(False)
+        Me.TabBogi.PerformLayout()
+        Me.GroupLieder.ResumeLayout(False)
+        Me.GroupLieder.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabBogi As Microsoft.Office.Tools.Ribbon.RibbonTab
+    Friend WithEvents GroupLieder As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents ButtonInsertSong As Microsoft.Office.Tools.Ribbon.RibbonButton
+    Friend WithEvents ButtonChangeDirectory As Microsoft.Office.Tools.Ribbon.RibbonButton
+End Class
+
+Partial Class ThisRibbonCollection
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Friend ReadOnly Property Ribbon() As Ribbon
+        Get
+            Return Me.GetRibbon(Of Ribbon)()
+        End Get
+    End Property
+End Class
---
+++ PPT Bogi/Ribbon.vb
@@ -0,0 +1,54 @@
+﻿Imports Microsoft.Office.Tools.Ribbon
+
+Public Class Ribbon
+
+    Private Sub Ribbon_Load(ByVal sender As System.Object, ByVal e As RibbonUIEventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ButtonInsertSong_Click(sender As Object, e As RibbonControlEventArgs) Handles ButtonInsertSong.Click
+
+        MySongFunctions.InsertSong()
+
+    End Sub
+
+    Private Sub ButtonChangeDirectory_Click(sender As Object, e As RibbonControlEventArgs) Handles ButtonChangeDirectory.Click
+
+        MySongFunctions.ChangeSongsDirectory()
+
+    End Sub
+
+    Private Sub GroupLieder_DialogLauncherClick(ByVal sender As Object, ByVal e As RibbonControlEventArgs) Handles GroupLieder.DialogLauncherClick
+
+        Dim helpInfo As String
+
+        helpInfo = "Mit diesem Add-In können Sie Lieder schneller in Ihre Präsentation einfügen." _
+            & vbNewLine & vbNewLine & vbNewLine
+
+        helpInfo &= "********************************************************************************" & vbNewLine
+        helpInfo &= "                                                  * Lied einfügen: *" & vbNewLine
+        helpInfo &= "********************************************************************************" & vbNewLine
+        helpInfo &= "Mit diesem Button können Sie direkt ein Lied nach der aktuellen Position einfügen." & vbNewLine
+        helpInfo &= "Als Eingabe geben Sie bitte die gewünschte Lied Nummer ein (z.B. 187)." & vbNewLine
+        helpInfo &= "Falls Sie ein Lied, welches als 'a' und 'b' Teil existiert, geben Sie die Nummer mit " _
+            & "anschließend dem Buchstaben ein (z.B. 85b)." & vbNewLine
+        helpInfo &= "Falls Sie Lieder mit verschiedenen Sprachen, nur bestimmten Strophen, oder alternativem Text" _
+                & "einzufügen wollen, müssen Sie dies leider manuell machen." _
+                & vbNewLine & vbNewLine
+
+        helpInfo &= "********************************************************************************" & vbNewLine
+        helpInfo &= "                                              * Verzeichnis ändern: *" & vbNewLine
+        helpInfo &= "********************************************************************************" & vbNewLine
+        helpInfo &= "Mit diesem Button legen Sie fest, aus welchem Verzeichnis die Lieder geöffnet werden sollen." & vbNewLine
+        helpInfo &= "Geben Sie dazu den absoluten Ordnerpfad an (z.B. ""C:\Users\Max\Documents\Lieder"")." _
+            & vbNewLine & vbNewLine & vbNewLine
+
+        helpInfo &= "Sollten Sie Probleme oder weitere Fragen haben, wenden Sie sich bitte an Joel Kovar." & vbNewLine
+        helpInfo &= "joel.m.kovar@gmail.com"
+
+
+        MsgBox(helpInfo, MsgBoxStyle.OkOnly, "Hilfe 'PPT Bogi Add-In v1.0'")
+
+    End Sub
+
+End Class
